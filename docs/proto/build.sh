b9f67098aae281e28fd7f125186243bf35c3ae28
wangfeng <wangfengxy@sina.cn>
2022-05-09 00:37:17 +0800

    修订protobuf协议转golang代码的脚本, 自动修订原版proto文件的package

--- docs/proto/build.sh
+++ docs/proto/build.sh
@@ -3,11 +3,22 @@ for file in ./*.proto
 do
     if test -f $file
     then
+        echo $file
         new_dir=${file%.*}
         d1=`basename ${new_dir}`
         dest=../../protocol/${d1}
-        mkdir $dest
+        new_package=`basename $file`
+        echo $new_package
+        new_package=${new_package%.*}
+        echo $new_package
+        package=${new_package//_/}
+        #echo ${package,,}
+        package=`echo ${package} | tr 'A-Z' 'a-z'`
+        echo $package
+        #mkdir $dest
         #echo ${dest}
+        sed -i -e "s/github.com\/futuopen\/ftapi4go\/pb\/${package}/github.com\/mymmsc\/futu4g\/protocol\/${new_package}/g" $file
+        rm ${file}-e
         protoc -I=./ --go_out=../../ $file
     fi
     if test -d $file
